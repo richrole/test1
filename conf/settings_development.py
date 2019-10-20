f49a76f0457872fb3e010ec81683aa8076bfe3fb
Rich <1129163932@qq.com>
2019-10-20 10:54:15 +0800

    ggg

--- conf/settings_development.py
+++ conf/settings_development.py
@@ -13,8 +13,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': 'bk-rich',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                        # 你的数据库user
-        'PASSWORD': 'root',                        # 你的数据库password
-        'HOST': 'localhost',                   # 开发的时候，使用localhost
+        'PASSWORD': 'L*vIW=.lTr',                        # 你的数据库password
+        'HOST': '192.168.165.181',                   # 开发的时候，使用localhost
         'PORT': '3306',                        # 默认3306
     },
 }
